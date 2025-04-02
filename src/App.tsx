f651b522ddf515670d56c952f3c026ff8aa7596f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-02 12:25:16 +0100

    refactor App component structure; remove RootLayout and index.css for cleaner setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import { BrowserRouter } from "react-router";
 import Router from "./pages/router";
-import RootLayout from "./pages/layout";
 export default function App() {
   return (
     <BrowserRouter>
-      <RootLayout>
-        <Router />
-      </RootLayout>
+      <Router />
     </BrowserRouter>
   );
 }
